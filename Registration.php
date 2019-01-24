f0cc7226906deee6dd4b42b713e07a7d00ec0de9
UK1aO13CV <hpd12@live.co.uk>
2019-01-24 11:43:04 +0000

    Update Registration.php

--- Registration.php
+++ Registration.php
@@ -1,4 +1,4 @@
-<?php include('C:\Users\hpd12\Desktop\PHPTest\PHPDBTEST.php');?>
+<?php include('C:\Users\hpd12\Desktop\MonoclGitHubRepo\MonoclBackend\PHPDBTEST.php');?>
 <!DOCTYPE html>
 <html>
 <link href="Assets/HomePageAssets.css" rel="stylesheet" type="text/css">
@@ -6,7 +6,7 @@
 <link href="Assets/CommonAssets.css" rel="stylesheet" type="text/css">
 <div id="MonocleHeaderBar">
 </div>
-<form method="post" action="C:\Users\hpd12\Desktop\PHPTest\PHPDBTEST.php"> 
+<form method="post" action="C:\Users\hpd12\Desktop\MonoclGitHubRepo\MonoclBackend\PHPDBTEST.php"> 
     <fieldset id="LogInBox">
         <legend>Sign Up</legend>
         <b>Enter Username:</b>
@@ -30,7 +30,7 @@
         <b>Password Confirmation:<b>
         <input align="texttop" type="text" name="PwdConfirmation">
         <input type="submit"id="NextButton">
-                    </fieldset>
+        </fieldset>
 </form>
 
 </html>
