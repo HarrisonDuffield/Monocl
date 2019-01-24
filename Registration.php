81a2936c7b2db1b0fb5e6eb64fca09401ae0a24f
UK1aO13CV <hpd12@live.co.uk>
2019-01-24 10:44:45 +0000

    HTML File Replaced/to incorporate PHP
    PHP FIle Replaced

---
+++ Registration.php
@@ -0,0 +1,36 @@
+<?php include('C:\Users\hpd12\Desktop\PHPTest\PHPDBTEST.php');?>
+<!DOCTYPE html>
+<html>
+<link href="Assets/HomePageAssets.css" rel="stylesheet" type="text/css">
+<link href="Assets/FontAssets.css" rel="stylesheet" type="text/css">
+<link href="Assets/CommonAssets.css" rel="stylesheet" type="text/css">
+<div id="MonocleHeaderBar">
+</div>
+<form method="post" action="C:\Users\hpd12\Desktop\PHPTest\PHPDBTEST.php"> 
+    <fieldset id="LogInBox">
+        <legend>Sign Up</legend>
+        <b>Enter Username:</b>
+        <input align="middle" type="text" name="UserName">
+        <br>
+        <b>Enter First Name:<b>
+        <input align="texttop" type="text" name="FirstName">
+        <br>
+        <b>Enter Last Name:<b>
+        <input align="texttop" type="text" name="LastName">
+        <br>
+        <b>Enter Email Address:<b>
+        <input align="texttop" type="text" name="Email">
+        <br>
+        <b>Enter Class Code(If Available):<b>
+        <input align="texttop" type="text" name="ClassCode">
+        <br>
+        <b>Create a Password:<b>
+        <input align="texttop" type="text" name="PwdInput">
+        <br>
+        <b>Password Confirmation:<b>
+        <input align="texttop" type="text" name="PwdConfirmation">
+        <input type="submit"id="NextButton">
+                    </fieldset>
+</form>
+
+</html>
