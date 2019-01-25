03514504c1b54801dfa18cbe1301ba22e92235f0
UK1aO13CV <hpd12@live.co.uk>
2019-01-25 11:46:39 +0000

    Update Registration.php
    Changed to add ID's to the inputs
    
    File directory changed as part of trying to get the inputs to input
    
    Web server now needed

--- Registration.php
+++ Registration.php
@@ -6,30 +6,30 @@
 <link href="Assets/CommonAssets.css" rel="stylesheet" type="text/css">
 <div id="MonocleHeaderBar">
 </div>
-<form method="post" action="C:\Users\hpd12\Desktop\MonoclGitHubRepo\MonoclBackend\PHPDBTEST.php"> 
+<form method="post" action="../MonoclBackend/PHPDBTEST.php"> 
     <fieldset id="LogInBox">
         <legend>Sign Up</legend>
         <b>Enter Username:</b>
-        <input align="middle" type="text" name="UserName">
+        <input align="middle" type="text" name="UserName" id="UserName">
         <br>
         <b>Enter First Name:<b>
-        <input align="texttop" type="text" name="FirstName">
+        <input align="texttop" type="text" name="FirstName" id="FirstName">
         <br>
         <b>Enter Last Name:<b>
-        <input align="texttop" type="text" name="LastName">
+        <input align="texttop" type="text" name="LastName" id="LastName">
         <br>
         <b>Enter Email Address:<b>
-        <input align="texttop" type="text" name="Email">
+        <input align="texttop" type="text" name="Email" id="Email">
         <br>
         <b>Enter Class Code(If Available):<b>
-        <input align="texttop" type="text" name="ClassCode">
+        <input align="texttop" type="text" name="ClassCode" id="ClassCode">
         <br>
         <b>Create a Password:<b>
-        <input align="texttop" type="text" name="PwdInput">
+        <input align="texttop" type="text" name="PwdInput" id="PwdInput">
         <br>
         <b>Password Confirmation:<b>
-        <input align="texttop" type="text" name="PwdConfirmation">
-        <input type="submit"id="NextButton">
+        <input align="texttop" type="text" name="PwdConfirmation" id="PwdConfirmation">
+        <input type="submit" value="Sign Up" id="NextButton">
         </fieldset>
 </form>
 
