ad6983698d0b14dc7fc76678fabb1c3f6564ec4a
UK1aO13CV <hpd12@live.co.uk>
2019-01-27 20:59:36 +0000

    Log In page created
    Just as a basic outline - basic throwaway

---
+++ LogIn.php
@@ -0,0 +1,20 @@
+<!DOCTYPE HTML>
+<html>
+<link href="Assets/HomePageAssets.css" rel="stylesheet" type="text/css">
+<link href="Assets/FontAssets.css" rel="stylesheet" type="text/css">
+<link href="Assets/CommonAssets.css" rel="stylesheet" type="text/css">
+<div id="MonocleHeaderBar">
+    <img src="Assets/Monocl.png" align="left" size="small">
+</div>
+<a> Log In!</a>
+<form method="POST" action>
+    <fieldset id="LogInBox" align="center">
+        <b> Enter Username</b>
+        <input type="text" align="middle" name="UserName" id="UserName">
+        <br>
+        <b> Enter Password</b>
+        <input type="text" align="texttop" name="Password" id="Password">
+        <br>
+        <input type="submit" value="Log In !" id="NextButton">
+    </fieldset>
+</html>
