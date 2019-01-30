6a574fe3187b24f0888c2edfde44dfe0b46fd969
UK1aO13CV <hpd12@live.co.uk>
2019-01-30 22:04:12 +0000

    Update LogIn.php
    Working HTML wise on a fucntioanl level  - the changes need to get this part working will be cosmetic / php based

--- LogIn.php
+++ LogIn.php
@@ -1,20 +1,26 @@
+<?php include("..\MonoclBackend\LoginDB.php") ?>
 <!DOCTYPE HTML>
 <html>
+
 <link href="Assets/HomePageAssets.css" rel="stylesheet" type="text/css">
 <link href="Assets/FontAssets.css" rel="stylesheet" type="text/css">
 <link href="Assets/CommonAssets.css" rel="stylesheet" type="text/css">
 <div id="MonocleHeaderBar">
-    <img src="Assets/Monocl.png" align="left" size="small">
+    <img src="Assets/Monocl.png" align="left" size="tiny">
 </div>
 <a> Log In!</a>
-<form method="POST" action>
+<form method="POST" action="LogIn.php">
+    <?php include("..\MonoclBackend\Errors.php");?>
     <fieldset id="LogInBox" align="center">
         <b> Enter Username</b>
-        <input type="text" align="middle" name="UserName" id="UserName">
+        <input type="text" align="middle" name="UserName" id="UserName" value="<?php echo $UserName; ?>">
         <br>
         <b> Enter Password</b>
-        <input type="text" align="texttop" name="Password" id="Password">
+        <input type="text" align="texttop" name="Password" id="Password"  value="<?php echo $Password; ?>">
         <br>
-        <input type="submit" value="Log In !" id="NextButton">
+        <input type="submit" value="Log In !" id="NextButton" name="LoginButtonGreen" >
+        <br>
+        <a href="\Registration.php"> Don't Have an account? Sign Up!</link>
     </fieldset>
+</form>
 </html>
