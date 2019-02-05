7d81469d23e0d6f0213a685a81f659afdc3e97f6
MonocledMajesticProductions <hpd12@live.co.uk>
2019-02-05 18:38:59 +0000

    Update AccountVerified.php

--- AccountVerified.php
+++ AccountVerified.php
@@ -1,15 +1,24 @@
 <html>
 <title> Account Verification | Monocl </title>
-<a>Account Verification</a>    
-<a><?php  
+<link href="Assets/FontAssets.css" rel="stylesheet" type="text/css">
+<link href="Assets/CommonAssets.css" rel="stylesheet" type="text/css">
+<link href="Assets/MainPageAssets.css" rel="stylesheet" type="text/css">
+<div id="MonocleHeaderBar">
+    <img src="Assets/Monocl.png" align="left">
+</div>
+<a>Account Verification</a>
+
+<b><?php  
+
 require("C:\Users\hpd12\Desktop\MonoclGitHubRepo\MonoclBackend\EmailVerification.php");
 $MessageToPrint = "";
 if(!empty($_GET['VerifyString'])){
 if(TableUpdate($_GET['VerifyString'])){
-    $MessageToPrint = "Account Activation success";
+    $MessageToPrint = "Account Activation success - now log in on the log in page";
+    
 }
 else{
-    $MessageToPrint = "Please Try Again";
+    $MessageToPrint = "Please Try Again - now redirecting to the log in page";
 }
 
 }
@@ -17,7 +26,7 @@ else{
     $MessageToPrint = "Error : No account information given to verify - please check your emails for the log in information";
 }
 echo $MessageToPrint;
-?> </a>
+?> </b>
 
 
 </html>
