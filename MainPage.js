a76a248e6d62bad8d8d300c6da683e7a86d1ffdf
MonocledMajesticProductions <hpd12@live.co.uk>
2019-03-08 14:13:10 +0000

    Goes with previous commit with the AJAX now working

--- MainPage.js
+++ MainPage.js
@@ -1,25 +1,64 @@
+
 window.onload = function(){
     alert("Main Page Js File Loaded");
 }
+$(document).ready(function(){
+    $("#test").click(function(e){
+        alert("hello");
+    })
+})
+
 function ClassLeaderBoard(){
     console.log("Class Leader Board Button Registed Click");
     $.ajax({
-        url:'C:\Users\hpd12\Desktop\MonoclGitHubRepo\MonoclBackend\AJAXtest.php',
+        url:'InterfaceLayer/LeaderBoardClass.php',
         type:"POST",
+        dataType: 'html',
         beforeSend:function(){
             $("f").text("test");
         },
-         success:function(data){
-            alert(data);
-            $("f").text(data); 
+         success: function(result,status,xhr){
+            alert(result);
+            $("theTable").text(result); 
+            console.log(result);
+            },
+            error: function(xhr,status,error){
+                alert("well this just didnt work did it");
             }
             
         }
 
 
     )
-    console.log("The ends");};
+    console.log("The ends");}
+    
 function PublicLeaderBoard(){
-    alert("Button word");
+    $('#Output').load('C:\Users\hpd12\Desktop\MonoclGitHubRepo\MonoclBackend\AJAXtest.php',function(response,success){
+        if(status==success){
+            alert("Did this work");
+        }
+        else{
+            alert("this did not work");
+        }
+        
+        console.log("no");
+    });
+    $("f").text("Button word");
     console.log("click");
+}
+function testbed(){
+    console.log("called");
+    $.ajax(
+        {
+            url: 'InterfaceLayer/response.php',
+            type: 'get',
+            dataType:'html',
+            error:function(response){
+            console.log(url);
+            },
+            success:function(response){
+                window.alert(response);
+                console.log("Woring");
+            }
+        })
 }
