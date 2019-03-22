e9759f2d08ae5e2a940cea0e5a915f5cd6e0f5b1
MonocledMajesticProductions <hpd12@live.co.uk>
2019-03-22 11:09:16 +0000

    Mid end case swithc sysem to load multiple funcitons4 the ajax - loopole

--- InterfaceLayer/TeacherDisplayInterface.php
+++ InterfaceLayer/TeacherDisplayInterface.php
@@ -2,8 +2,20 @@
 session_start();
 $UserID = $_SESSION["UserLoggedIn"];
 $FunctionToUse = $_POST["FunctionToSelect"];
+$HomeworkID = $_POST["HomeworkID"];
 require("..\..\MonoclBackend\Leaderboard.php");
+require("..\..\MonoclBackend\QuestionTablePopulator.php");
+
 if($FunctionToUse == 1){
     LeaderBoardDisplayClass($UserID);
+}
+else if($FunctionToUse ==2){
+    WhoHasCompletedHomwork($HomeworkID);
+    //get the homworks where complete;
+}
+else if($FunctionToUse ==3){
+    MarkAsDone($HomeworkID);
+}
+else{
 }
 
